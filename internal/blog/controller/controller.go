679c2da271232d63b80c0566f130143b76d70e36
Arlindo Neto <neto.acs@gmail.com>
2025-06-09 20:03:26 -0400

    added blog controller

---
+++ internal/blog/controller/controller.go
@@ -0,0 +1,49 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BlogController struct {
+}
+
+func NewBlogController() *BlogController {
+	return &BlogController{}
+}
+
+func RegisterBlogRoutes(router *gin.Engine) {
+	bc := NewBlogController()
+
+	blogGroup := router.Group("/blog")
+	{
+		blogGroup.GET("/", bc.GetPosts)
+		blogGroup.GET("/:id", bc.GetPostByID)
+		blogGroup.POST("/", bc.CreatePost)
+		// Add more user routes
+	}
+
+	router.GET("/blog", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "blog.html", gin.H{
+			"title": "Contact",
+		})
+	})
+
+}
+
+// GetUsers handles GET /users
+func (bc *BlogController) GetPosts(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Get all users"})
+}
+
+// GetUserByID handles GET /users/:id
+func (bc *BlogController) GetPostByID(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "Get user by ID: " + id})
+}
+
+// CreateUser handles POST /users
+func (bc *BlogController) CreatePost(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"message": "Create a new user"})
+}
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "salada/internal/routes"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"salada/internal/blog/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	routes.GetRouter()
+	router := gin.Default()
+
+	router.Static("/assets/", "./web/assets")
+	router.Static("/images/", "./web/images")
+	router.LoadHTMLGlob("web/templates/html/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Home",
+		})
+	})
+	router.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about.html", gin.H{
+			"title": "About",
+		})
+	})
+
+	router.GET("/contact", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "contact.html", gin.H{
+			"title": "Contact",
+		})
+	})
+
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", gin.H{
+			"title": "404",
+		})
+	})
+
+	router.NoMethod(func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"code": "METHOD_NOT_ALLOWED", "message": "405 method not allowed"})
+	})
+
+	controller.RegisterBlogRoutes(router)
+
+	bindIp := fmt.Sprintf("%s:8080", os.Getenv("BIND_IP"))
+	router.Run(bindIp)
 }
