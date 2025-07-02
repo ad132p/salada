e27c7661487b81ea1f461c76da17fb4ba36ba032
Arlindo Neto <neto.acs@gmail.com>
2025-07-02 18:21:07 -0400

    quadlet files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
+# Create database secret:
+POSTGRES_ROOT_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+kubectl create secret generic \
+    --from-literal=password="${POSTGRES_ROOT_PASSWORD}" \
+    postgres-root-password-kube \
+    --dry-run=client \
+    -o yaml | \
+    podman kube play -
+
+# Create a registry if you dont have one
+podman container run -dt -p 5000:5000 --name registry docker.io/library/registry:2
+# Build and pushsalada image
 podman build -t salada .
-podman run --name salada -d -p 8080:8080 localhost/salada
-podman run -p 5432:5432 --name salada-db -e POSTGRES_PASSWORD=password -d postgres
-psql -d "host=localhost port=5432 dbname=postgres user=postgres"
+podman image tag localhost/salada localhost:5000/salada:latest
+podman image push localhost:5000/salada:latest --tls-verify=false
+
+#Database migration
+psql -d "host=localhost port=5432 dbname=postgres user=postgres" < internal/db/databases.sql
+psql -d "host=localhost port=5432 dbname=salada user=postgres" internal/db/tables.sql
