ea18f3746601d24f54e5e589be02a97bc5c0f455
Arlindo Neto <neto.acs@gmail.com>
2025-11-09 09:29:04 +0000

    placed middleware into new route only

--- internal/routes/blog_router.go
+++ internal/routes/blog_router.go
@@ -8,7 +8,7 @@ import (
 )
 
 func BlogRoutes(router *gin.Engine, blogController *controller.BlogController) {
-	postRoutes := router.Group("/blog", middleware.AuthenticateMiddleware)
+	postRoutes := router.Group("/blog")
 	postRoutes.Use(middleware.DBLoggerMiddleware())
 	{
 		postRoutes.GET("/", blogController.GetPosts)
@@ -20,7 +20,7 @@ func BlogRoutes(router *gin.Engine, blogController *controller.BlogController) {
 		postRoutes.POST("/like", blogController.LikePost)
 		postRoutes.DELETE("/:id", blogController.DeletePost)
 		postRoutes.PUT("/:id", blogController.UpdatePost)
-		postRoutes.GET("/new", blogController.GetNewPostForm)
+		postRoutes.GET("/new", middleware.AuthenticateMiddleware, blogController.GetNewPostForm)
 		postRoutes.GET("/edit/:slug", blogController.EditPostForm)
 		postRoutes.PATCH("/publish/:id", blogController.PublishPost)
 		postRoutes.GET("/category/:name", blogController.GetCategory)
